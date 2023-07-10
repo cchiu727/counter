5caba97aee03adf593aa54de3a6e463c418f231d
Collin Chiu <61135705+cchiu727@users.noreply.github.com>
2023-07-10 11:49:45 -0700

    Add Counter object for OOP approach

--- counter.js
+++ counter.js
@@ -1,29 +1,53 @@
 const decreaseButton = document.getElementById('decrease-button');
 const resetButton = document.getElementById('reset-button');
 const increaseButton = document.getElementById('increase-button');
-const counter = document.getElementById('counter');
+const counterLabel = document.getElementById('counter');
 
-let counterNum = counter.innerHTML;
+class Counter {
+    constructor() {
+        this.value = 0;
+        this.class = '';
+    }
 
-function decrCounter() {
-    counterNum--;
-    counter.innerHTML = counterNum;
+    setClass() {
+        if (this.value > 0) {
+            counterLabel.className = 'green';
+        }
+        else if (this.value < 0) {
+            counterLabel.className = 'red';
+        }
+        else {
+            counterLabel.className = '';
+        }
+    }
+
+    updateCounterLabel(val) {
+        counterLabel.innerHTML = val;
+    }
+
+    updateValue(step) {
+        this.value += step;
+        this.setClass();
+        this.updateCounterLabel(this.value);
+    }
+
+    reset() {
+        this.value = 0;
+        this.setClass();
+        this.updateCounterLabel(this.value);
+    }
 }
 
-function incrCounter() {
-    counterNum++;
-    counter.innerHTML = counterNum;
-}
+let counter = new Counter();
 
 decreaseButton.addEventListener('click', function() {
-    decrCounter();
+    counter.updateValue(-1);
 });
 
 resetButton.addEventListener('click', function() {
-    counterNum = 0;
-    counter.innerHTML = counterNum;
+    counter.reset();
 });
 
 increaseButton.addEventListener('click', function() {
-    incrCounter();
+    counter.updateValue(1);
 });
